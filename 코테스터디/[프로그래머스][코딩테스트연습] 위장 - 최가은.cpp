c1d1b6978e47689a25a92b941bb6b9851e950b6b
Kang JY <jyk2367@naver.com>
2021-11-19 19:27:10 +0900

    21.11.19

---
+++ 코테스터디/[프로그래머스][코딩테스트연습] 위장 - 최가은.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    unordered_map<string, int> map;
+    for(int i=0; i<clothes.size(); i++)
+        map[clothes[i][1]]++;
+    
+    for(auto s: map)
+        answer*=(s.second+1);
+    
+    return --answer;
+}
