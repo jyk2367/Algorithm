d9844e1e13bbb87dcfaff92806229f3bc85fb7c7
Kang JY <jyk2367@naver.com>
2022-01-15 12:06:34 +0900

    22.1.15

---
+++ 백준/[백준]11055 - 가장 큰 증가 부분 수열.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int answer;
+
+int A[1002];
+int dp[1002];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	for(int i = 1; i <=N;i++) {
+		cin >> A[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+
+		int sum = 0;
+		for (int j = 1; j < i; j++) {
+			if(A[j]<A[i])
+				sum = max(sum, dp[j]);
+		}
+		dp[i] = sum+A[i];
+		answer = max(answer,dp[i]);
+	}
+
+
+	cout << answer << "\n";
+
+	return 0;
+
+}
