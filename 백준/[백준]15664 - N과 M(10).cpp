73f1e09fa93f8fd1b5a669faa4ea9d04d79286bc
Kang JY <jyk2367@naver.com>
2022-03-19 17:04:02 +0900

    22.3.19

---
+++ 백준/[백준]15664 - N과 M(10).cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int N, M;
+
+void func(int k,int idx,int last,vector<int> &v,vector<int> &answer) {
+	if (k >= M) {
+		for (int i = 0; i < M; i++) {
+			cout << answer[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int before = -1;
+	for (int i = idx; i < N; i++) {
+		if (last <= v[i]&& before!=v[i]) {
+			before=v[i];
+			answer.push_back(v[i]);
+			func(k+1,i+1, v[i], v, answer);
+			answer.pop_back();
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M;
+
+	vector<int> v,answer;
+	
+	for (int i = 0; i < N; i++) {
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+	//void func(int k,int idx,int last,vector<int> &v,vector<int> &answer) 
+	func(0,0,-1,v, answer);
+
+
+	return 0;
+}
