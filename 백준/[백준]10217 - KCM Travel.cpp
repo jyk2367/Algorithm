94599f31e07290655cfe881ce5d7682f005b05ac
Kang JY <jyk2367@naver.com>
2022-07-25 07:47:36 +0900

    22.07.25

--- 백준/[백준]10217 - KCM Travel.cpp
+++ 백준/[백준]10217 - KCM Travel.cpp
@@ -5,42 +5,35 @@
 #define INF 0x3f3f3f3f
 
 using namespace std;
-
 struct Ticket {
 	int Arrival;
 	int Cost;
 	int Time;
 
-	Ticket(int v,int c,int t):Arrival(v),Cost(c),Time(t) {	}
+	Ticket(int v, int c, int t) :Arrival(v), Cost(c), Time(t) {	}
 };
 
 struct t_comp {
 	bool operator()(const Ticket& t1, const Ticket& t2) {
 		if (t1.Time == t2.Time) {
-					return t1.Cost > t2.Cost;
+			return t1.Cost > t2.Cost;
 		}
 		return t1.Time > t2.Time;
 	}
 };
 
-
 int T, N, M, K;
 int u, v, c, d;
 
-priority_queue<Ticket, vector<Ticket>, t_comp> pq;
 vector<Ticket> airport[102];
 int t[102][10002];
 
-
 void Init() {
-	for (int i = 0; i < 102; i++) {
+	for (int i = 1; i <=N; i++) {
 		airport[i].clear();
-		for(int j = 0; j <= 10000; j++) {
+		for (int j = 1; j <=M; j++) {
 			t[i][j] = INF;
 		}
-	}
-	while (!pq.empty()) {
-		pq.pop();
 	}
 }
 
@@ -52,14 +45,17 @@ int main() {
 
 	cin >> T;
 	while (T--) {
-		Init();
 		cin >> N >> M >> K;
+
+		Init();
+
 		for (int i = 0; i < K; i++) {
 			cin >> u >> v >> c >> d;
 			//u에서 출발, v로 가면서 비용은 c, 소요시간은 d
-			airport[u].push_back(Ticket(v,c, d));
+			airport[u].push_back(Ticket(v, c, d));
 		}
 
+		priority_queue<Ticket, vector<Ticket>, t_comp> pq;
 		pq.push(Ticket(1, 0, 0));
 		t[1][0] = 0;
 
@@ -68,6 +64,7 @@ int main() {
 			int nowCost = pq.top().Cost;
 			int nowTime = pq.top().Time;
 			pq.pop();
+
 			if (t[now][nowCost] < nowTime)
 				continue;
 			if (nowCost > M)
@@ -75,7 +72,7 @@ int main() {
 			if (now == N)
 				break;
 
-			for (Ticket ticket : airport[now]) {
+			for (Ticket const& ticket : airport[now]) {
 				int next = ticket.Arrival;
 				int nextCost = nowCost + ticket.Cost;
 				int nextTime = nowTime + ticket.Time;
@@ -85,7 +82,10 @@ int main() {
 						if (t[next][j] > nextTime) {
 							t[next][j] = nextTime;
 						}
+						else
+							break;
 					}
+					t[next][nextCost] = nextTime;
 					pq.push(Ticket(next, nextCost, nextTime));
 				}
 			}
@@ -93,17 +93,17 @@ int main() {
 
 
 		int answer = 0x7fff'ffff;
-		for (int i = 1; i <=M; i++) {
+		for (int i = 1; i <= M; i++) {
 			answer = min(answer, t[N][i]);
 		}
-		
-		if(answer ==INF)
+
+		if (answer == INF)
 			cout << "Poor KCM" << "\n";
 		else
 			cout << answer << "\n";
 
 	}
-	
+
 
 	return 0;
 }
